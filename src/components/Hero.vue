c8e17728c351069576d8c72362b7f7f75614377b
raihan <raihannismara@gmail.com>
2020-05-01 17:33:28 +0700

    changed image hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,12 +5,12 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1">
-          <h1>Masjid Al-Anhar</h1>
-          <h2>Masjid digital pertama di Indonesia berlokasi di Daerah Istimewa Yogyakarta</h2>
+          <h1>{{profile.mosque.name}}</h1>
+          <h2>Berlokasikan di {{profile.mosque.address}} {{profile.mosque.country}}</h2>
           <router-link :to="{name:'Donate'}" class="btn-get-started scrollto">Bantu Kami</router-link>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img">
-          <img src="../assets/bg-msq.png" class="img-fluid animated float-right" height="300" width="300" alt="">
+          <img src="../assets/masjid.png" class="img-fluid animated float-right" height="300" width="300" alt="">
         </div>
       </div>
     </div>
@@ -19,8 +19,14 @@
 </template>
 
 <script>
+import profiles from '@/constants/profile'
 export default {
-  name: 'Hero'
+  name: 'Hero',
+  data(){
+    return {
+      profile: profiles
+    }
+  }
 }
 </script>
 
